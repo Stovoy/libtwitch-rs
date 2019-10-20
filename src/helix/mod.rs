6cd8f2c50f994eee3c896baa950742f658508bc1
simonsan <simon@systemli.org>
2019-10-20 05:31:05 +0200

    Preparation for "Helix"

---
+++ src/helix/mod.rs
@@ -0,0 +1,13 @@
+#[macro_use]
+pub mod analytics;
+pub mod bits;
+pub mod clips;
+pub mod entitlements;
+pub mod games;
+pub mod moderation;
+pub mod streams;
+pub mod subscriptions;
+pub mod tags;
+pub mod users;
+pub mod videos;
+pub mod webhooks;
